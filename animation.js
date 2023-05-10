a2cc6cbe583d278880b11b92bfa817cb7b0e3ee0
Corey Clarke <coreydclarke@icloud.com>
2023-05-10 16:37:36 -0700

    Module 4 Assignment update

---
+++ animation.js
@@ -0,0 +1,11 @@
+var toggleButton = document.getElementById("activate_drop");
+var content = document.getElementById("java_drop_active");
+
+toggleButton.addEventListener("click", function() {
+    console.log(content.style.display)
+  if (content.style.display === "none") {
+    content.style.display = "block";
+  } else {
+    content.style.display = "none";
+  }
+});
