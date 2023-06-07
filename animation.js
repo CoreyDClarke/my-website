c6784e1653d35fce62a811198211320bd1b2c6f2
Corey Clarke <coreydclarke@icloud.com>
2023-06-06 20:09:45 -0700

    Update with new configurations,visuals, content and bug fixes

--- animation.js
+++ animation.js
@@ -1,9 +1,9 @@
 var toggleButton = document.getElementById("activate_drop");
 var content = document.getElementById("java_drop_active");
 
-toggleButton.addEventListener("click", function() {
-    console.log(content.style.display)
-  if (content.style.display === "none") {
+toggleButton.addEventListener("click", function () {
+  console.log(content.style.display)
+  if (!content.style.display || content.style.display === "none") {
     content.style.display = "block";
   } else {
     content.style.display = "none";
